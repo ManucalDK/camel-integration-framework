0821216e1d71d75fc68f6f1a9c93af8128be253a
manuel.calambas <manuel.calambas@ceiba.com.co>
2021-04-28 09:02:18 -0500

    feat: new bean

--- camel-microservice-a/src/main/java/com/cameltest/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
+++ camel-microservice-a/src/main/java/com/cameltest/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
@@ -6,7 +6,12 @@
 package com.cameltest.microservices.camelmicroservicea.routes.a;
 
 import java.time.LocalDateTime;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,32 +20,60 @@ import org.springframework.stereotype.Component;
  * @author manuc
  */
 @Component
-public class MyFirstTimerRouter extends RouteBuilder{
+public class MyFirstTimerRouter extends RouteBuilder {
 
-    @Autowired
-    private GetCurrentTimeBean getCurrentTimeBean;
-    
-    @Override
-    public void configure() throws Exception {
-        //(timer) escuchar cola endpoint 1
-        //transformacion
-        //(log) almacenar en base de datos endpoint 2
-        
-        from("timer:first-timer") //timer endpoint
-        .log("${body}")
-        .transform().constant("My constant message")
-        .log("${body}")
+
+
+	@Autowired
+	private GetCurrentTimeBean getCurrentTimeBean;
+	
+	@Autowired
+	private SimpleLoggingProccessingComponent logginComponent;
+
+	@Override
+	public void configure() throws Exception {
+		// (timer) escuchar cola endpoint 1
+		// transformacion
+		// (log) almacenar en base de datos endpoint 2
+
+		from("timer:first-timer") // timer endpoint
+				.transform().constant("My constant message")
+				.log("${body}")
 //        .transform().constant("Time now is: "+ LocalDateTime.now())
-        .bean(getCurrentTimeBean, "getCurrentTime")
-        .log("${body}")
-        .to("log:first-timmer");
-    }
-    
+				.bean(getCurrentTimeBean, "getCurrentTime")
+				.log("${body}")
+				.bean(logginComponent, "process")
+				.log("${body}")
+				.process(new SimpleLoggingProcessor())
+				.to("log:first-timmer");
+	}
+
 }
 
 @Component
-class GetCurrentTimeBean{
-    public String getCurrentTime(){
-        return "Time now is: "+ LocalDateTime.now();
-    }
+class GetCurrentTimeBean {
+	public String getCurrentTime() {
+		return "Time now is: " + LocalDateTime.now();
+	}
+}
+
+@Component
+class SimpleLoggingProccessingComponent {
+	
+	private Logger logger = LoggerFactory.getLogger(SimpleLoggingProccessingComponent.class);
+	
+	public void process(String message) {
+		logger.info("SimpleLoggingProccessingComponent {}", message);
+	}
+}
+
+class SimpleLoggingProcessor implements Processor {
+	
+	private Logger logger = LoggerFactory.getLogger(SimpleLoggingProccessingComponent.class);
+
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		logger.info("SimpleLoggingProccessingComponent {}", exchange.getMessage().getBody());
+	}
+
 }
